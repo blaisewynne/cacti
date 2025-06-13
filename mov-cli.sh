deaff9b94405301e2f6727e7bcaef404a71c8120
bmw <blaise.wynne1@gmail.com>
2025-06-13 23:28:47 +1000

    Version 0.2

--- mov-cli.sh
+++ mov-cli.sh
@@ -1,31 +1,40 @@
 CYAN='\033[0;36m'
 NORMAL='\033[0m'
-LBLUE='\033[1;34m'
-LPURPLE='\033[1;35m'
+LBLUE='\033[0;34m'
+LPURPLE='\033[0;35m'
+GREEN='\033[0;32m'
 
-version=0.1
+NC='\033[0m'
+
+version=0.2
 
 main () {
-	printf "${LBLUE}===========================================================================================================\n"
-	printf "${CYAN}                                                 Mov-Cli ${NORMAL}\n"
-	printf "${LBLUE}===========================================================================================================\n"
-	printf "${CYAN}[Mov-Cli]\n"
+	printf "${CYAN}[Mov-Cli] ${NC}\n"
+	printf "${LBLUE}Search for a movie using the -p command:${NC}\n"
 	read input
+	printf "${NORMAL}"
 	case $input in
 
-		test)
-			printf "Test!"
+		test | Test)
+			printf "Test!\n"
 		;;
-		help)
+		-h | --help | -help | help)
 			help_info
 
 		;;
-		-v)
+		-v | --version | -version | version)
 			version_info
 		;;
+		-p | --play | -play | play)
+			play_video
+		;;
+		-i | --info | -info | info)
+			program_info
+		;;
+
 
 	*)
-		printf "Command not found. Use command help for assistance.\n"
+		printf "Command not found. Use command -help for assistance.${NC}\n"
 esac
 }
 
@@ -40,15 +49,33 @@ help_info () {
 	-v, --version
 	   Check current version of Mov-Cli
 	-h, --help
-	   Show current help message	
+	   Show current help message
+   	-p, --play
+	   Play a movie	
 	"
 	echo ""
 	printf "${NORMAL}"
 }
 
 version_info () {
-	printf "${CYAN}Current Version: ${LBLUE}$version\n"
+	printf "${CYAN}Current Version:${LBLUE}$version\n"
 	printf "${NORMAL}"
+
+}
+
+play_video () {
+	
+	printf "${CYAN}Please enter ${LPURPLE}video link.${NORMAL}\n"
+	read url
+	printf "${CYAN}${url}${NC}\n"
+	mpv $url
+
+}
+
+program_info () {
+
+	printf "${GREEN}This program was made by a humble programmer named Blaise Wynne${NORMAL}\n"
+
 
 }
 
